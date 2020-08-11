a4e93e911b73f3e755db8776f6eb0c7151c22410
zjb1132325494 <1132325494@qq.com>
2020-08-11 08:23:42 +0800

    任务二

---
+++ test1/Test1/Test1/Controllers/ValuesController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RabbitSqlLib;
+
+namespace Test1.Controllers
+{
+    [Microsoft.AspNetCore.Cors.EnableCors("AllowAllOrigins")]
+    [Route("api/Values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet("testget")]
+        public ActionResult TestGet([FromQuery] List<string> bzjh)
+        {
+            //RabbitAccess access = GetAccess();
+            //DataTable dt = access.GetDataTable("select * from ts_j_basicinfo where well_id='DQtFbHtP6C'");
+            return Ok(bzjh);
+        }
+
+        public RabbitAccess GetAccess()
+        {
+            DBEntity dBEntity = new DBEntity();
+            dBEntity.DBType = "oracle";
+            dBEntity.DBPort = "1521";
+            dBEntity.DBServer = "132.232.16.136";
+            dBEntity.DBName = "orcl";
+            dBEntity.DBUser = "dqts";
+            dBEntity.DBPwd = "dqts";
+            string dBType = "";
+            string connstr = dBEntity.GetConnStr(out dBType);
+            RabbitAccess access = new RabbitAccess(dBType, connstr);
+            return access;
+        }
+
+    }
+}
